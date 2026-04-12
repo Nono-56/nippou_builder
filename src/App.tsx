cda1da2170dfc5cf1f2fd52f973deaff02c5b403
Nono <br23024@sic.shibaura-it.ac.jp>
2026-04-12 20:40:25 +0900

    fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { EntryForm } from './components/EntryForm';
 import { TaskList } from './components/TaskList';
 import { ReportPreview } from './components/ReportPreview';
